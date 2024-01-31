2e558e83d441299108311e07dffc3368ae33e842
Sebastian Jokela <sebastian.jokela@tuni.fi>
2024-01-31 16:57:42 +0200

    Extended hook timeout to hopefully ensure that timeouts don't cause problems when running tests

--- tests/vitest.config.js
+++ tests/vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ['./vitest.setup.js'],
-    hookTimeout: 90000
+    hookTimeout: 120000
   },
 });
